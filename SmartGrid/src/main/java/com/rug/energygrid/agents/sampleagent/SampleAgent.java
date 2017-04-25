fa81ed3a4b62f2ac4f95bfdd5d2242030fc79e31
Thijs <knaap@live.com>
2017-04-25 14:05:16 +0200

    first jade tutorial, working

---
+++ SmartGrid/src/main/java/com/rug/energygrid/agents/sampleagent/SampleAgent.java
@@ -0,0 +1,21 @@
+package com.rug.energygrid.agents.sampleagent;
+import jade.core.Agent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by thijs on 25-4-17.
+ */
+public class SampleAgent extends Agent {
+    private static final Logger logger = LoggerFactory.getLogger(SampleAgent.class);
+
+    @Override
+    public void setup() {
+        //final String otherAgentName = (String) this.getArguments()[0];
+        System.out.println("Hello! "+getAID().getName()+ " is my name!");
+    }
+
+    @Override
+    public void takeDown() {
+    }
+}
