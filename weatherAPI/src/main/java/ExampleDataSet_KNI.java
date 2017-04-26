6ed7f48d19ea71bee92c889a39be2b8f30ef038a
Ruben <Gjr.kip@gmail.com>
2017-04-26 23:04:21 +0200

    Added start weather

---
+++ weatherAPI/src/main/java/ExampleDataSet_KNI.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Ruben on 26-Apr-17.
+ */
+
+//Static example, API would call specific dataSet in each method
+public class ExampleDataSet_KNI extends Weather {
+
+    public ExampleDataSet_KNI(){
+        super();
+        this.dataSet = readDataSet("weatherDataKNI", ","); //This is done because of Constant set
+    }
+
+    //These methods change depending on constant set or API
+    public double getWindSpeed(int time) {
+        return dataSet.get(time).get(WIND_SPEED_POS);
+    }
+
+    public int getWindDirection(int time) {
+        return dataSet.get(time).get(WIND_DIRECTION_POS);
+    }
+
+    public double getSunIrradiation(int time) {
+        return dataSet.get(time).get(SOLAR_IRRIDIANCE_POS);
+    }
+
+
+    //These are the values that have to be set depending on your dataset indexing
+    public int getTimePos() {
+        return 1;
+    }
+
+    public int getWindDirPos() {
+        return 2;
+    }
+
+    public int getWindSpeedPos() {
+        return 3;
+    }
+
+    public int getSolarIrrPos() {
+        return 20;
+    }
+}
